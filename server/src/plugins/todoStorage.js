46d8fae490245e12011ea8d4c96a91737dd9ec95
Akira-Demizu <1992a1114@gmail.com>
2020-12-15 18:24:22 +0900

    #3 todoStorageでidを管理しないように修正

--- server/src/plugins/todoStorage.js
+++ server/src/plugins/todoStorage.js
@@ -8,7 +8,6 @@ const todoStorage = {
     todos.forEach(function (todo, index) {
       todo.id = index
     })
-    todoStorage.uid = todos.length
     return todos
   },
   save: function (todos) {
